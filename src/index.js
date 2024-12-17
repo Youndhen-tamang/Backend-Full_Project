e2f4e0c126160f22a74531494fa3796d2c41d1ed
Youndhen <161960615+Youndhen@users.noreply.github.com>
2024-12-17 21:43:16 +0545

    connection to database

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// require('dotenv').config({path:'./env'})
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path:'./env'
+})
+connectDB();
+/*
+import express from "express"
+
+const app = express();
+;(async()=>{
+  try{
+   await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+   app.on("error",(error)=>{
+    console.log("ERRR:",error);
+    throw error
+   })
+
+   app.listen(process.env.PORT,()=>{ 
+    console.log(`APP is Listiening on port ${process.env.PORT}`)
+   })
+  }catch{
+    console.error("Error:",error)
+    throw err
+  }
+})()*/

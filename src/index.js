fccc82e72071e96d961015865290d87ddf06023e
Youndhen <161960615+Youndhen@users.noreply.github.com>
2024-12-18 17:05:03 +0545

    api debugging

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from 'dotenv'
 import connectDB from "./db/index.js";
+import app from './app.js';
 
 dotenv.config({
   path:'./env'
 })
-connectDB();
+connectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 8000),()=>{
+    console.log(`Server is runnimg at port: ${process.env.PORT}`)
+    
+  }
+
+
+})
+.catch((err)=>{
+  console.log("MongoDB connection failed",err)
+  app.on("error",(error)=>{
+    console.log("ERRR:",error);
+    throw error
+   })
+})
 /*
 import express from "express"
 
